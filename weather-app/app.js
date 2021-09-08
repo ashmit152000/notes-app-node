72172867bd5def4f46ac0b5d653c7e4d808b5da0
Ashmit Pathak <ashmitpathak@Ashmits-MacBook-Air.local>
2021-09-08 11:19:55 +0530

    added changes

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,18 +1,24 @@
 
 const getDataOnAddress = require('./utils/geocode.js')
 const weatherData = require('./utils/weather.js')
-
+const chalk = require('chalk')
 
 
 
 const address = process.argv[2]
 
-getDataOnAddress(address,(error,lat, lon, place_name) => {
-    if(error){
-        return console.log(error)
-    }else{
-        weatherData(lat,lon, place_name)
-    }
+if(!address){
+    return console.log(chalk.red.inverse('Address not provided'))
+}else{
+    getDataOnAddress(address,(error,lat, lon, place_name) => {
+        if(error){
+            return console.log(error)
+        }else{
+            weatherData(lat,lon, place_name)
+        }
+    
+    })
+}
 
-})
+
 
