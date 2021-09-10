1ed4e56214daa85b9101006715e9e619343657ff
Ashmit Pathak <ashmitpathak@Ashmits-MacBook-Air.local>
2021-09-10 10:59:16 +0530

    changes in web server

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,6 +6,19 @@ app.get('', (req, res) => {
     res.send('Hello, Express')
 })
 
+app.get('/about', (req, res) => {
+    res.send('This is the about page')
+})
+
+app.get('/weather', (req, res) => {
+    res.send('This is the weather app')
+})
+
+
+app.get('/help', (req, res) => {
+    res.send('This is the help page')
+})
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 })
