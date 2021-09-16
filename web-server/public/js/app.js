c7a74055ce661f3f4e78f0f6e12f01d31b7d340f
Ashmit Pathak <ashmitpathak@Ashmits-MacBook-Air.local>
2021-09-16 20:22:32 +0530

    added the ui

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -4,16 +4,22 @@ console.log('The Javascript part is done!!')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 weatherForm.addEventListener('submit',(e) => {
     e.preventDefault()
     const location = search.value
     fetch('http://localhost:3000/weather?address=' + search.value).then((response) => {
     response.json().then((data) => {
+        messageOne.textContent = 'Loading...'
+        messageTwo.textContent = ''
         if(data.error) {
-            console.log(data.error)
+            messageTwo.textContent = data.error
         }else {
             console.log(data.location)
             console.log(data.forecast)
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
         }
     })
 })
